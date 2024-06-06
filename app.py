05d2a1fc94f86aa952f6300d7f0cffc30683b0ea
Oyewo Temidayo <oyewodayo@gmail.com>
2024-06-06 08:47:35 +0100

    Upload pdf and spit text in chunks

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from streamlit_extras.add_vertical_space import add_vertical_space
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
 
 # sidebar content
 with st.sidebar:
@@ -12,4 +15,30 @@ with st.sidebar:
     - [OpenAI](https://platform.openai.com/docs/models) LLM model
     ''')
     add_vertical_space(5)
-    st.write('Made by [Temidayo Oyewo](https://x.xom/oyewoday)')
+    st.write('Made by [Temidayo Oyewo](https://x.com/oyewoday)')
+
+def main():
+    st.header("Chat with your file")
+    uploaded_pdf = st.file_uploader("Upload your file", type='pdf')
+
+   
+    if uploaded_pdf is not None:
+        pdf_reader = PdfReader(uploaded_pdf)
+
+        # num_pages = len(pdf_reader.pages)
+        # st.write(f"Uploaded file: {uploaded_pdf.name}")
+        # st.write(f"The PDF file has {num_pages} pages.")
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text=text)
+        st.write(chunks)
+
+if __name__ == '__main__':
+    main()
